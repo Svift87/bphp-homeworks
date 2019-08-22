4a7759d16dbf4cca90621c3785c0ca4c8bbd8888
Svift <kuryakov-s@mail.ru>
2019-08-23 00:15:59 +0700

    test form1

--- bphp-diplom/index.php
+++ bphp-diplom/index.php
@@ -11,15 +11,11 @@ function login($users) {
     $login = isset($_POST['login']) ? $_POST['login'] : '';
     if (array_key_exists($login, $users)) {
         if ($_POST['login'] == 'manager' && $_POST['password'] == $users['manager']) {
-            echo 'ok manager!';
-            // header('Location: /manager/manager.html');
-            // exit;
-            return true;
+            header('Location: /bphp-diplom/manager/manager.html');
+            exit;
         } else if ($_POST['login'] == 'translator' && $_POST['password'] == $users['translator']) {
-            echo 'ok translator!';
-            // header('Location: /translator/translator.html');
-            // exit;
-            return true;
+            header('Location: /bphp-diplom/translator/translator.html');
+            exit;
         }
     }
 
